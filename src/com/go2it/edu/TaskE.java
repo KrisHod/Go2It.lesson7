fe9fe1ca9e711f44e4282c2b4d0b081d98d0cea3
Kris <krisgodom@gmail.com>
2020-12-19 23:27:24 -0400

    added TaskE

---
+++ src/com/go2it/edu/TaskE.java
@@ -0,0 +1,29 @@
+package com.go2it.edu;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class TaskE {
+    public static void readInfo() {
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream("Cat.java");
+            fis.read();
+        } catch (FileNotFoundException e) {
+            e.getMessage();
+        } catch (IOException e) {
+            e.getMessage();
+        } finally {
+            try {
+                fis.close();
+            } catch (IOException | NullPointerException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        readInfo();
+    }
+}
